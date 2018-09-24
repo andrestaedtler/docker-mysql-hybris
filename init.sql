3e7e0ad0a76d7fa4dffb222ab6d4a62bef35ed9e
andrestaedtler <32069266+andrestaedtler@users.noreply.github.com>
2018-09-24 15:34:27 +0200

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,3 +1,3 @@
-CREATE USER 'hybris_admin'@'%' IDENTIFIED BY 'password';
+CREATE USER 'hybris_admin'@'%' IDENTIFIED BY 'hybris';
 CREATE DATABASE hybris;
 GRANT ALL PRIVILEGES ON hybris.* TO 'hybris_admin'@'%' WITH GRANT OPTION;
